394f4e7e5cddda11850c8aa12dc62407c9c39537
MoriokaReimen <kei.nakata.job@gmail.com>
2024-05-04 16:37:18 +0900

    [update] middle of implementation

--- app/main.py
+++ app/main.py
@@ -1,5 +1,26 @@
+import kivy
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+
+class MainLayout(BoxLayout):
+    pass
+
+    def press_f1(self, instance):
+        print("F1")
+
+    def press_f2(self, instance):
+        print("F2")
+
+    def search_location(self):
+        print(f"{self.search_input.text}")
+
+class MainApp(App):
+    def build(self):
+        return MainLayout()
+
 def main()-> None:
-    print("Hello")
+    MainApp().run()
 
 if __name__ == '__main__':
     main()
